e939d9bb1d4fb5c17244b561979cf6dac7023b13
Matthias Geier <Matthias.Geier@gmail.com>
2026-01-28 20:43:34 +0100

    ptrs, interleaved

--- src/lib.rs
+++ src/lib.rs
@@ -98,7 +98,7 @@ unsafe impl<T, Inner: Deref<Target = [T]>> IntoChannelPtrs for &[Inner] {
             .map(Deref::deref)
             .map(<[T]>::len)
             .reduce(|a, b| {
-                assert_eq!(a, b, "all channels must have equal length");
+                assert_eq!(a, b, "all channels must have the same length");
                 a
             })
             .unwrap_or(0);
@@ -173,7 +173,7 @@ unsafe impl<T, Inner: Deref<Target = [T]>, const N: usize> IntoChannelPtrs for [
             .map(Deref::deref)
             .map(<[T]>::len)
             .reduce(|a, b| {
-                assert_eq!(a, b, "all channels must have equal length");
+                assert_eq!(a, b, "all channels must have the same length");
                 a
             })
             .unwrap_or(0);
@@ -218,7 +218,7 @@ impl<T> ChannelPtrsBoxed<T> {
             .map(Deref::deref)
             .map(<[T]>::len)
             .reduce(|a, b| {
-                assert_eq!(a, b, "all channels must have equal length");
+                assert_eq!(a, b, "all channels must have the same length");
                 a
             })
             .unwrap_or(0);
@@ -236,6 +236,137 @@ impl<T> ChannelPtrsBoxed<T> {
     // TODO: try to re-assign slice with different lifetime (see https://github.com/mgeier/rsor)
 }
 
+// TODO: errors instead of panics?
+
+pub fn channel_ptrs_from_slices_mut<T, Channel, Channels>(
+    signal: Channels,
+    storage: &mut [*mut T],
+) -> (*mut *mut T, usize, u16)
+where
+    Channel: AsMut<[T]>,
+    Channels: IntoIterator<Item = Channel>,
+{
+    let mut signal = signal.into_iter();
+    let mut frames = None;
+    let channels = signal
+        .by_ref()
+        .zip(storage.iter_mut())
+        .map(|(mut ch, ptr)| {
+            //channels += 1;
+            let ch = ch.as_mut();
+            let current_frames = ch.len();
+            if let Some(f) = frames {
+                assert_eq!(current_frames, f, "all channels must have the same length");
+            } else {
+                frames = Some(current_frames);
+            }
+            *ptr = ch.as_mut_ptr();
+        })
+        .count()
+        .try_into()
+        .expect("too many channels");
+    assert!(signal.next().is_none(), "too few pointers in `storage`");
+    (storage.as_mut_ptr(), frames.unwrap_or(0), channels)
+}
+
+pub unsafe fn channel_ptrs_to_slices_mut<'a, 'b, T>(
+    ptrs: *mut *mut T,
+    frames: usize,
+    channels: u16,
+) -> &'a mut [&'b mut [T]] {
+    todo!()
+}
+
+// TODO: move to tests (or examples?)
+
+#[derive(Default)]
+pub struct Processor {
+    channel_ptrs: [*mut f32; 6],
+}
+
+unsafe extern "C" fn do_nothing(_: *mut *mut f32, _: usize, _: u16) {}
+
+impl Processor {
+    // NB: This takes a mutable reference because it is *not* reentrant.
+    // TODO: explain lifetimes ('b could be longer than 'a)
+    pub fn process<'a, 'b, Channel, Channels>(&'a mut self, signal: Channels) -> &'a mut [&'b mut [f32]]
+    where
+        Channel: AsMut<[f32]> + 'b,
+        Channels: IntoIterator<Item = Channel>,
+    {
+        let (ptrs, frames, channels) = channel_ptrs_from_slices_mut(signal, &mut self.channel_ptrs);
+
+        // Let's pretend that we are passing `ptrs` to some FFI function here,
+        // where the channel data will be overwritten.
+        // SAFETY: Doing nothing is safe.
+        unsafe {
+            do_nothing(ptrs, frames, channels);
+        }
+
+        // SAFETY: Results from `channel_ptrs_from_slices_mut()` are valid for the given lifetimes.
+        unsafe { channel_ptrs_to_slices_mut(ptrs, frames, channels) }
+    }
+}
+
+pub fn process_iter<Channel, Channels>(signal: Channels) -> usize
+where
+    Channel: AsMut<[f32]>,
+    Channels: IntoIterator<Item = Channel>,
+{
+    let mut channels = 0;
+    let mut frames = None;
+    let signal = signal.into_iter();
+    for mut ch in signal {
+        channels += 1;
+        let current_frames = ch.as_mut().len();
+        if let Some(f) = frames {
+            assert_eq!(current_frames, f, "all channels must have the same length");
+        } else {
+            frames = Some(current_frames);
+        }
+        //ch.as_mut()[0] = 99.0;
+    }
+    channels
+}
+
+// TODO: copy_to_interleaved_uninit()
+
+pub fn copy_to_interleaved<T, Channel, Channels>(source: Channels, destination: &mut [T])
+where
+    T: Copy,
+    Channel: AsRef<[T]>,
+    Channels: IntoIterator<IntoIter: ExactSizeIterator, Item = Channel>,
+{
+    let source = source.into_iter();
+    let mut frames = None;
+    // TODO: get channels from dest_len / frames and avoid ExactSizeIterator?
+    // TODO: check if there are too many or too few channels
+    let channels = source.len();
+    for (offset, ch) in source.enumerate() {
+        let ch = ch.as_ref();
+        let current_frames = ch.len();
+        if let Some(f) = frames {
+            assert_eq!(current_frames, f, "all channels must have the same length");
+        } else {
+            // TODO: better error message?
+            assert_eq!(
+                current_frames * channels,
+                destination.len(),
+                "length mismatch"
+            );
+            frames = Some(current_frames);
+        }
+        for (dst, src) in destination
+            .iter_mut()
+            .skip(offset)
+            .step_by(channels)
+            .zip(ch)
+        {
+            *dst = *src;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -249,6 +380,49 @@ mod tests {
         // This "pointer to pointers" would typically be passed to some C API.
         let _frames = ptrs.frames();
         let _channels = ptrs.channels();
+    }
+
+    #[test]
+    fn iter_from_slice() {
+        let signal: &mut [&mut [_]] = &mut [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
+        assert_eq!(process_iter(signal), 2);
+    }
+
+    #[test]
+    #[cfg(feature = "alloc")]
+    fn iter_from_vec() {
+        let signal = vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]];
+        assert_eq!(process_iter(signal), 2);
+    }
+
+    #[test]
+    fn iter_from_array() {
+        let signal: [&mut [_]; _] = [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
+        assert_eq!(process_iter(signal), 2);
+    }
+
+    #[test]
+    fn iter_from_chunks() {
+        let back_to_back: &mut [_] = &mut [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+        assert_eq!(process_iter(back_to_back.chunks_mut(3)), 2);
+    }
+
+    #[test]
+    fn test_copy_to_interleaved() {
+        let source: [&mut [_]; _] = [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
+        let mut destination = [0.0; 6];
+        copy_to_interleaved(source, &mut destination);
+        assert_eq!(destination, [1.0, 4.0, 2.0, 5.0, 3.0, 6.0]);
+    }
+
+    #[test]
+    fn process_slice() {
+        let signal: &mut [&mut [_]] = &mut [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
+        {
+            let mut p = Processor::default();
+            let result = p.process(signal);
+            assert_eq!(result, [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]);
+        }
     }
 
     #[test]
