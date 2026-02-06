37bce2900cd89c7e7d1909987145b509e11176e6
Matthias Geier <Matthias.Geier@gmail.com>
2026-02-06 20:02:31 +0100

    errors instead of (most) panics

--- src/lib.rs
+++ src/lib.rs
@@ -14,36 +14,42 @@ extern crate alloc;
 const MAX_CHANNELS_FROM_SLICE: usize = 16;
 */
 
-// TODO: errors instead of panics?
-
-pub fn channel_ptrs_from_slices_mut<T, Channel, Channels>(
+/// ...
+///
+/// # Panics
+///
+/// If the requested number of channels doesn't fit into `u16`.
+pub fn channel_ptrs_from_slices_mut<T, Channels>(
     signal: Channels,
     storage: &mut [*mut T],
-) -> (*mut *mut T, usize, u16)
+) -> Result<(*mut *mut T, usize, u16), Error>
 where
-    Channel: AsMut<[T]>,
-    Channels: IntoIterator<Item = Channel>,
+    Channels: IntoIterator<Item: AsMut<[T]>>,
 {
     let mut signal = signal.into_iter();
     let mut frames = None;
     let channels = signal
         .by_ref()
         .zip(storage.iter_mut())
-        .map(|(mut ch, ptr)| {
+        .try_fold(0usize, |acc, (mut ch, ptr)| {
             let ch = ch.as_mut();
             let current_frames = ch.len();
             if let Some(f) = frames {
-                assert_eq!(current_frames, f, "all channels must have the same length");
+                if current_frames != f {
+                    return Err(Error::Jagged);
+                }
             } else {
                 frames = Some(current_frames);
             }
             *ptr = ch.as_mut_ptr();
-        })
-        .count()
+            Ok(acc + 1)
+        })?
         .try_into()
         .expect("too many channels");
-    assert!(signal.next().is_none(), "too few pointers in `storage`");
-    (storage.as_mut_ptr(), frames.unwrap_or(0), channels)
+    if signal.next().is_some() {
+        return Err(Error::StorageOverflow);
+    }
+    Ok((storage.as_mut_ptr(), frames.unwrap_or(0), channels))
 }
 
 /// Slices from pointers ...
@@ -56,16 +62,18 @@ pub unsafe fn channel_ptrs_to_slices_mut<'a, 'b, T>(
     frames: usize,
     channels: u16,
     storage: &'a mut [*mut [T]],
-) -> &'a mut [&'b mut [T]] {
+) -> Result<&'a mut [&'b mut [T]], Error> {
     let channels = channels.into();
-    assert!(channels <= storage.len(), "not enough space in `storage`");
+    if storage.len() < channels {
+        return Err(Error::StorageOverflow);
+    }
     for (i, channel_slice) in storage.iter_mut().enumerate().take(channels) {
         // SAFETY: Caller must ensure requirements stated in docstring.
         let s = unsafe { core::slice::from_raw_parts_mut(*ptrs.add(i), frames) };
         *channel_slice = s;
     }
     // SAFETY: The correct number of slices has been initialized above.
-    unsafe { core::slice::from_raw_parts_mut(storage.as_mut_ptr() as *mut &mut [_], channels) }
+    Ok(unsafe { core::slice::from_raw_parts_mut(storage.as_mut_ptr() as *mut &mut [_], channels) })
 }
 
 /// Something something.
@@ -119,12 +127,12 @@ unsafe extern "C" fn set_a_value(ptrs: *mut *mut f32, frames: usize, channels: u
 
 impl Processor {
     // NB: This takes a mutable reference to `self` because it is *not* reentrant.
-    pub fn process<Channel, Channels>(&mut self, signal: Channels)
+    pub fn process<Channels>(&mut self, signal: Channels)
     where
-        Channel: AsMut<[f32]>,
-        Channels: IntoIterator<Item = Channel>,
+        Channels: IntoIterator<Item: AsMut<[f32]>>,
     {
-        let (ptrs, frames, channels) = channel_ptrs_from_slices_mut(signal, &mut self.channel_ptrs);
+        let (ptrs, frames, channels) =
+            channel_ptrs_from_slices_mut(signal, &mut self.channel_ptrs).unwrap();
 
         // SAFETY: channel_ptrs_from_slices_mut() returned valid results.
         unsafe {
@@ -133,35 +141,19 @@ impl Processor {
     }
 }
 
-// TODO: turn into example (or remove?)
-pub fn process_iter<Channel, Channels>(signal: Channels) -> usize
-where
-    Channel: AsMut<[f32]>,
-    Channels: IntoIterator<Item = Channel>,
-{
-    let mut channels = 0;
-    let mut frames = None;
-    let signal = signal.into_iter();
-    for mut ch in signal {
-        channels += 1;
-        let current_frames = ch.as_mut().len();
-        if let Some(f) = frames {
-            assert_eq!(current_frames, f, "all channels must have the same length");
-        } else {
-            frames = Some(current_frames);
-        }
-        //ch.as_mut()[0] = 99.0;
-    }
-    channels
-}
-
 // TODO: copy_to_interleaved_uninit()
 
-pub fn copy_to_interleaved<T, Channel, Channels>(source: Channels, destination: &mut [T])
+// TODO: copy_to_noninterleaved()? use memcpy?
+
+// TODO: noninterleaved_to_* via chunks()/chunks_mut()?
+
+pub fn copy_to_interleaved<T, Channels>(
+    source: Channels,
+    destination: &mut [T],
+) -> Result<(), Error>
 where
     T: Copy,
-    Channel: AsRef<[T]>,
-    Channels: IntoIterator<IntoIter: ExactSizeIterator, Item = Channel>,
+    Channels: IntoIterator<IntoIter: ExactSizeIterator, Item: AsRef<[T]>>,
 {
     let source = source.into_iter();
     let mut frames = None;
@@ -172,14 +164,13 @@ where
         let ch = ch.as_ref();
         let current_frames = ch.len();
         if let Some(f) = frames {
-            assert_eq!(current_frames, f, "all channels must have the same length");
+            if current_frames != f {
+                return Err(Error::Jagged);
+            }
         } else {
-            // TODO: better error message?
-            assert_eq!(
-                current_frames * channels,
-                destination.len(),
-                "length mismatch"
-            );
+            if current_frames * channels != destination.len() {
+                return Err(Error::LengthMismatch);
+            }
             frames = Some(current_frames);
         }
         for (dst, src) in destination
@@ -191,6 +182,19 @@ where
             *dst = *src;
         }
     }
+    Ok(())
+}
+
+// TODO: multiple errors? rename?
+#[derive(Debug)]
+pub enum Error {
+    // all channels must have the same length
+    Jagged,
+    // TODO: not all functions need this
+    LengthMismatch,
+    // TODO: not all functions need this
+    // too few pointers in `storage`
+    StorageOverflow,
 }
 
 #[cfg(test)]
@@ -201,35 +205,10 @@ mod tests {
     use alloc::vec;
 
     #[test]
-    fn iter_from_slice() {
-        let signal: &mut [&mut [_]] = &mut [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
-        assert_eq!(process_iter(signal), 2);
-    }
-
-    #[test]
-    #[cfg(feature = "alloc")]
-    fn iter_from_vec() {
-        let signal = vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]];
-        assert_eq!(process_iter(signal), 2);
-    }
-
-    #[test]
-    fn iter_from_array() {
-        let signal: [&mut [_]; _] = [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
-        assert_eq!(process_iter(signal), 2);
-    }
-
-    #[test]
-    fn iter_from_chunks() {
-        let back_to_back: &mut [_] = &mut [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-        assert_eq!(process_iter(back_to_back.chunks_mut(3)), 2);
-    }
-
-    #[test]
     fn test_copy_to_interleaved() {
         let source: [&mut [_]; _] = [&mut [1.0, 2.0, 3.0], &mut [4.0, 5.0, 6.0]];
         let mut destination = [0.0; 6];
-        copy_to_interleaved(source, &mut destination);
+        copy_to_interleaved(source, &mut destination).unwrap();
         assert_eq!(destination, [1.0, 4.0, 2.0, 5.0, 3.0, 6.0]);
     }
 
@@ -248,7 +227,7 @@ mod tests {
     }
 
     #[test]
-    fn from_array() {
+    fn process_array() {
         let ch0 = [1.0, 2.0, 3.0];
         let ch1 = [4.0, 5.0, 6.0];
         let mut p = Processor::new();
@@ -264,9 +243,18 @@ mod tests {
         }
     }
 
+    // stacked == non-interleaved
+    #[test]
+    fn process_stacked() {
+        let mut data = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+        let mut p = Processor::new();
+        p.process(data.chunks_mut(3));
+        assert_eq!(data, [99.9, 2.0, 3.0, 4.0, 5.0, 6.0]);
+    }
+
     // Mono signals can be put into a one-element array.
     #[test]
-    fn from_single_channel() {
+    fn process_single_channel() {
         let mono = [1.0, 2.0, 3.0, 4.0];
         let mut p = Processor::new();
         // TODO: array is `Copy` so this copies the whole signal and modifies the copy!
