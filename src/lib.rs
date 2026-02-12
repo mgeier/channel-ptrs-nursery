4eef88a9da1f3da3465c5dec74815c65f24b1549
Matthias Geier <Matthias.Geier@gmail.com>
2026-02-12 21:08:15 +0100

    copy from/to ...

--- src/lib.rs
+++ src/lib.rs
@@ -342,69 +342,13 @@ impl Processor {
     }
 }
 
-// TODO: copy_to_noninterleaved()? use memcpy?
-
-// TODO: noninterleaved_to_* via chunks()/chunks_mut()?
-
-// TODO: test with frames = 0
-pub fn copy_to_interleaved<T>(
-    source: impl IntoIterator<IntoIter: ExactSizeIterator, Item: Channel<T>>,
-    destination: &mut [T],
-) -> Result<(), Error>
-where
-    T: Copy,
-{
-    // SAFETY: Transmuting &mut [T] to &mut [MaybeUninit<T>] is generally unsafe!
-    // However, T implements Copy and only valid T values will ever be written,
-    // and the reference never leaves our control, it should be fine.
-    let destination = unsafe { &mut *(destination as *mut [_] as *mut _) };
-    copy_to_interleaved_uninit(source, destination).map(|_| {})
-}
-
-pub fn copy_to_interleaved_uninit<T>(
-    source: impl IntoIterator<IntoIter: ExactSizeIterator, Item: Channel<T>>,
-    destination: &mut [MaybeUninit<T>],
-) -> Result<&mut [T], Error>
-where
-    T: Copy,
-{
-    let source = source.into_iter();
-    // TODO: move this comment to the docstring?
-    // NB: len() is provided by ExactSizeIterator.
-    // We could probably implement this without it, but it's simpler
-    // and we can show off how to get the number of channels from an iterator.
-    let channels = source.len();
-    let mut frames = None;
-    for (offset, ch) in source.enumerate() {
-        let ch = ch.as_ref();
-        let current_frames = ch.len();
-        if let Some(f) = frames {
-            if current_frames != f {
-                return Err(Error::Jagged);
-            }
-        } else {
-            if current_frames * channels != destination.len() {
-                return Err(Error::LengthMismatch);
-            }
-            frames = Some(current_frames);
-        }
-        for (dst, src) in destination
-            .iter_mut()
-            .skip(offset)
-            .step_by(channels)
-            .zip(ch)
-        {
-            *dst = MaybeUninit::new(*src);
-        }
-    }
-    // TODO: return frames & channels?
-    // SAFETY: All slice elements have been initialized.
-    Ok(unsafe {
-        core::slice::from_raw_parts_mut(destination.as_mut_ptr().cast(), destination.len())
-    })
-}
-
-// refer to copy_to_interleaved for ExactSizeIterator etc.
+/// Copies all samples from a single slice of interleaved channels into contiguous channels.
+///
+/// # Errors
+///
+/// [`Error::Jagged`] if not all destination channels have the same length.
+/// [`Error::LengthMismatch`] if the samples don't fit snugly into the destination.
+// TODO: refer to copy_to_interleaved for ExactSizeIterator etc.
 pub fn copy_from_interleaved<T>(
     source: &mut [T],
     destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<T>>,
@@ -412,28 +356,7 @@ pub fn copy_from_interleaved<T>(
 where
     T: Copy,
 {
-    copy_from_interleaved_and_return_iter(source, destination).try_for_each(|ch| ch.map(|_| ()))
-}
-
-pub fn copy_from_interleaved_uninit<T>(
-    source: &mut [T],
-    destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<MaybeUninit<T>>>,
-) -> Result<(), Error>
-where
-    T: Copy,
-{
-    copy_from_interleaved_and_return_iter_uninit(source, destination)
-        .try_for_each(|ch| ch.map(|_| ()))
-}
-
-pub fn copy_from_interleaved_and_return_iter<T>(
-    source: &mut [T],
-    destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<T>>,
-) -> impl ExactSizeIterator<Item = Result<&mut [T], Error>>
-where
-    T: Copy,
-{
-    copy_from_interleaved_and_return_iter_uninit(
+    copy_from_interleaved_uninit(
         source,
         destination.into_iter().map(|mut ch| {
             let ch = ch.as_mut();
@@ -443,7 +366,24 @@ where
     )
 }
 
-pub fn copy_from_interleaved_and_return_iter_uninit<T>(
+/// Copies all samples from a single slice of interleaved channels into contiguous uninitialized channels.
+///
+/// Same as [`copy_from_interleaved()`], but writing into uninitialized channels.
+pub fn copy_from_interleaved_uninit<T>(
+    source: &mut [T],
+    destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<MaybeUninit<T>>>,
+) -> Result<(), Error>
+where
+    T: Copy,
+{
+    copy_from_interleaved_uninit_and_iterate(source, destination).try_for_each(|ch| ch.map(|_| ()))
+}
+
+/// Copies all samples from a single slice of interleaved channels into contiguous uninitialized
+/// channels and returns an iterator over now-initialized channels.
+///
+/// Same as [`copy_from_interleaved_uninit()`], but returning an iterator.
+pub fn copy_from_interleaved_uninit_and_iterate<T>(
     source: &mut [T],
     destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<MaybeUninit<T>>>,
 ) -> impl ExactSizeIterator<Item = Result<&mut [T], Error>>
@@ -453,7 +393,7 @@ where
     let destination = destination.into_iter();
     let channels = destination.len();
     let mut frames = None;
-    destination.enumerate().map(move |(offset, mut ch)| {
+    destination.enumerate().map(move |(i, mut ch)| {
         let ch = ch.as_mut();
         let current_frames = ch.len();
         if let Some(f) = frames {
@@ -468,12 +408,227 @@ where
         }
         for (dst, src) in ch
             .iter_mut()
-            .zip(source.iter_mut().skip(offset).step_by(channels))
+            .zip(source.iter_mut().skip(i).step_by(channels))
         {
             *dst = MaybeUninit::new(*src);
         }
         // SAFETY: TODO: see above?
         Ok(unsafe { core::slice::from_raw_parts_mut(ch.as_mut_ptr().cast(), ch.len()) })
+    })
+}
+
+/// Copies all samples from a single slice of non-interleaved channels into separate channels.
+///
+/// This is likely faster than [`copy_from_interleaved()`] because contiguous chunks can be copied.
+///
+/// It is likely even faster to not copy the channels at all.
+/// If a function accepts an iterator over (contiguous) channels,
+/// [`source.chunks()`](slice::chunks) can be used to create an appropriate iterator
+/// from a slice of non-interleaved channels.
+///
+/// # Errors
+///
+/// [`Error::Jagged`] if not all destination channels have the same length.
+/// [`Error::LengthMismatch`] if the samples don't fit snugly into the destination.
+pub fn copy_from_noninterleaved<T>(
+    source: &mut [T],
+    destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<T>>,
+) -> Result<(), Error>
+where
+    T: Copy,
+{
+    copy_from_noninterleaved_uninit(
+        source,
+        destination.into_iter().map(|mut ch| {
+            let ch = ch.as_mut();
+            // SAFETY: TODO: same as above?
+            unsafe { core::slice::from_raw_parts_mut(ch.as_mut_ptr().cast(), ch.len()) }
+        }),
+    )
+}
+
+/// Copies all samples from a single slice of non-interleaved channels into separate uninitialized channels.
+///
+/// Same as [`copy_from_noninterleaved()`], but writing into uninitialized channels.
+pub fn copy_from_noninterleaved_uninit<T>(
+    source: &mut [T],
+    destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<MaybeUninit<T>>>,
+) -> Result<(), Error>
+where
+    T: Copy,
+{
+    copy_from_noninterleaved_uninit_and_iterate(source, destination)
+        .try_for_each(|ch| ch.map(|_| ()))
+}
+
+/// Copies all samples from a single slice of non-interleaved channels into separate uninitialized
+/// channels and returns an iterator over now-initialized channels.
+///
+/// Same as [`copy_from_noninterleaved_uninit()`], but returning an iterator.
+pub fn copy_from_noninterleaved_uninit_and_iterate<T>(
+    source: &mut [T],
+    destination: impl IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<MaybeUninit<T>>>,
+) -> impl ExactSizeIterator<Item = Result<&mut [T], Error>>
+where
+    T: Copy,
+{
+    let destination = destination.into_iter();
+    let channels = destination.len();
+    let mut frames = None;
+    destination.enumerate().map(move |(i, mut ch)| {
+        let ch = ch.as_mut();
+        let current_frames = ch.len();
+        if let Some(f) = frames {
+            if current_frames != f {
+                return Err(Error::Jagged);
+            }
+        } else {
+            if current_frames * channels != source.len() {
+                return Err(Error::LengthMismatch);
+            }
+            frames = Some(current_frames);
+        }
+        // SAFETY: Source and destination point to the right amount of elements
+        // and non-overlapping uninitialized space, respectively.
+        unsafe {
+            core::ptr::copy_nonoverlapping(
+                source.as_ptr().add(i * current_frames),
+                ch.as_mut_ptr().cast(),
+                current_frames,
+            );
+        }
+        // SAFETY: TODO: see above?
+        Ok(unsafe { core::slice::from_raw_parts_mut(ch.as_mut_ptr().cast(), ch.len()) })
+    })
+}
+
+/// Copies contiguous channels into a single slice, interleaving them.
+///
+/// # Errors
+///
+/// [`Error::Jagged`] if not all source channels have the same length.
+/// [`Error::LengthMismatch`] if the channels don't fit snugly into the destination.
+// TODO: test with frames = 0
+pub fn copy_to_interleaved<T>(
+    source: impl IntoIterator<IntoIter: ExactSizeIterator, Item: Channel<T>>,
+    destination: &mut [T],
+) -> Result<(), Error>
+where
+    T: Copy,
+{
+    // SAFETY: Transmuting &mut [T] to &mut [MaybeUninit<T>] is generally unsafe!
+    // However, T implements Copy and only valid T values will ever be written,
+    // and the reference never leaves our control, so it should be fine.
+    let destination = unsafe { &mut *(destination as *mut [_] as *mut _) };
+    copy_to_interleaved_uninit(source, destination).map(|_| {})
+}
+
+/// Copies contiguous channels into a single uninitialized slice, interleaving them.
+///
+/// Same as [`copy_to_interleaved()`], but writing into an uninitialized slice.
+///
+/// Returns an initialized version of the destination slice on success.
+pub fn copy_to_interleaved_uninit<T>(
+    source: impl IntoIterator<IntoIter: ExactSizeIterator, Item: Channel<T>>,
+    destination: &mut [MaybeUninit<T>],
+) -> Result<&mut [T], Error>
+where
+    T: Copy,
+{
+    let source = source.into_iter();
+    // TODO: move this comment to the docstring?
+    // NB: len() is provided by ExactSizeIterator.
+    // We could probably implement this without it, but it's simpler
+    // and we can show off how to get the number of channels from an iterator.
+    let channels = source.len();
+    let mut frames = None;
+    for (i, ch) in source.enumerate() {
+        let ch = ch.as_ref();
+        let current_frames = ch.len();
+        if let Some(f) = frames {
+            if current_frames != f {
+                return Err(Error::Jagged);
+            }
+        } else {
+            if current_frames * channels != destination.len() {
+                return Err(Error::LengthMismatch);
+            }
+            frames = Some(current_frames);
+        }
+        for (dst, src) in destination.iter_mut().skip(i).step_by(channels).zip(ch) {
+            *dst = MaybeUninit::new(*src);
+        }
+    }
+    // TODO: return frames & channels?
+    // SAFETY: All slice elements have been initialized.
+    Ok(unsafe {
+        core::slice::from_raw_parts_mut(destination.as_mut_ptr().cast(), destination.len())
+    })
+}
+
+/// Copies contiguous channels into a single slice, one after another.
+///
+/// # Errors
+///
+/// [`Error::Jagged`] if not all source channels have the same length.
+/// [`Error::LengthMismatch`] if the channels don't fit snugly into the destination.
+// TODO: Regarding ExactSizeIterator, see copy_to_interleaved()
+pub fn copy_to_noninterleaved<T>(
+    source: impl IntoIterator<IntoIter: ExactSizeIterator, Item: Channel<T>>,
+    destination: &mut [T],
+) -> Result<(), Error>
+where
+    T: Copy,
+{
+    // SAFETY: Transmuting &mut [T] to &mut [MaybeUninit<T>] is generally unsafe!
+    // However, T implements Copy and only valid T values will ever be written,
+    // and the reference never leaves our control, so it should be fine.
+    let destination = unsafe { &mut *(destination as *mut [_] as *mut _) };
+    copy_to_noninterleaved_uninit(source, destination).map(|_| ())
+}
+
+/// Copies contiguous channels into a single uninitialized slice, one after another.
+///
+/// Same as [`copy_to_noninterleaved()`], but writing into an uninitialized slice.
+///
+/// Returns an initialized version of the destination slice on success.
+pub fn copy_to_noninterleaved_uninit<T>(
+    source: impl IntoIterator<IntoIter: ExactSizeIterator, Item: Channel<T>>,
+    destination: &mut [MaybeUninit<T>],
+) -> Result<&mut [T], Error>
+where
+    T: Copy,
+{
+    let source = source.into_iter();
+    let channels = source.len();
+    let mut frames = None;
+    for (i, ch) in source.enumerate() {
+        let ch = ch.as_ref();
+        let current_frames = ch.len();
+        if let Some(f) = frames {
+            if current_frames != f {
+                return Err(Error::Jagged);
+            }
+        } else {
+            if current_frames * channels != destination.len() {
+                return Err(Error::LengthMismatch);
+            }
+            frames = Some(current_frames);
+        }
+        // SAFETY: Source and destination point to the right amount of elements
+        // and non-overlapping uninitialized space, respectively.
+        unsafe {
+            core::ptr::copy_nonoverlapping(
+                ch.as_ptr(),
+                destination.as_mut_ptr().add(i * current_frames).cast(),
+                current_frames,
+            );
+        }
+    }
+    // TODO: return frames & channels?
+    // SAFETY: All slice elements have been initialized.
+    Ok(unsafe {
+        core::slice::from_raw_parts_mut(destination.as_mut_ptr().cast(), destination.len())
     })
 }
 
