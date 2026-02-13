4327e8c07e8baa32107a56eadeec231eb4d3cf77
Matthias Geier <Matthias.Geier@gmail.com>
2026-02-13 20:11:26 +0100

    Add some (preliminary) docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,163 @@
 //! Pointers to channels (and more?).
 //!
 //! These are needed in some C APIs, do not use this in pure Rust code!
+//!
+//! TODO: move this to separate module?
+//!
+//!
+//! ... contiguous channels are common ...
+//!
+//! slice of slice is an obvious choice ...
+//!
+//! ```
+//! pub fn process(channels: &mut [&mut [f32]]) {
+//!     for channel in channels {
+//!         channel[0] *= 5.0;
+//!     }
+//! }
+//!
+//! let mut left = [0.1, 0.2, 0.3];
+//! let mut right = [-0.1, -0.2, -0.3];
+//! process(&mut [&mut left, &mut right]);
+//! assert_eq!(left, [0.5, 0.2, 0.3]);
+//! assert_eq!(right, [-0.5, -0.2, -0.3]);
+//! ```
+//!
+//! array/Vec does not work ...
+//!
+//! ```
+//! pub fn process(channels: &mut [impl AsMut<[f32]>]) {
+//!     for channel in channels {
+//!         let channel = channel.as_mut();
+//!         channel[0] *= 5.0;
+//!     }
+//! }
+//!
+//! let mut data = [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]];
+//! process(&mut data);
+//! assert_eq!(data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
+//!
+//! let mut vec_data = vec![vec![0.1, 0.2, 0.3], vec![-0.1, -0.2, -0.3]];
+//! process(&mut vec_data);
+//! assert_eq!(vec_data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
+//! ```
+//!
+//! This is a fine solution (with a reasonably simple implementation),
+//! but if we are willing to put in some more work, we can make our `process()` function
+//! even more flexible.
+//!
+//! But things will get worse before they get better again ...
+//!
+//! ```
+//! pub fn process(channels: impl IntoIterator<Item: AsMut<[f32]>>) {
+//!     for mut channel in channels {
+//!         let channel = channel.as_mut();
+//!         channel[0] *= 5.0;
+//!     }
+//! }
+//!
+//! let mut noninterleaved = [0.1, 0.2, 0.3, -0.1, -0.2, -0.3];
+//! process(noninterleaved.chunks_mut(3));
+//! assert_eq!(noninterleaved, [0.5, 0.2, 0.3, -0.5, -0.2, -0.3]);
+//!
+//! // however, this is problematic ...
+//!
+//! let mut data = [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]];
+//! process(data);
+//! assert_eq!(data, [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]]); // NO CHANGE!
+//!
+//! // note that the data is unchanged ... since array is `Copy` ...
+//! ```
+//!
+//! ... we can avoid this by adding more stuff ...
+//!
+//! ```
+//! pub fn process<'c, C: AsMut<[f32]> + ?Sized + 'c>(channels: impl IntoIterator<Item = &'c mut C>) {
+//!     for mut channel in channels {
+//!         let channel = channel.as_mut();
+//!         channel[0] *= 5.0;
+//!     }
+//! }
+//!
+//! let mut data = [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]];
+//! process(&mut data);
+//! assert_eq!(data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
+//!
+//! // The `?Sized` part above is needed for this to work:
+//! let mut noninterleaved = [0.1, 0.2, 0.3, -0.1, -0.2, -0.3];
+//! process(noninterleaved.chunks_mut(3));
+//! assert_eq!(noninterleaved, [0.5, 0.2, 0.3, -0.5, -0.2, -0.3]);
+//! ```
+//!
+//! ... this does what we want, but the function signature is getting quite cryptic ...
+//!
+//! ... let's try to make the function signature less cryptic by introducing a cryptic trait
+//! and a blanket implementation ...
+//!
+//! ```
+//! trait ChannelMut<T>: AsMut<[T]> {}
+//! impl<T, U: AsMut<[T]> + ?Sized> ChannelMut<T> for &mut U {}
+//!
+//! pub fn process(channels: impl IntoIterator<Item: ChannelMut<f32>>) {
+//!     for mut channel in channels {
+//!         let channel = channel.as_mut();
+//!         channel[0] *= 5.0;
+//!     }
+//! }
+//!
+//! // This allows all the usage scenarios we've seen before.
+//!
+//! let mut data = [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]];
+//! process(&mut data);
+//! assert_eq!(data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
+//!
+//! let mut vec_data = vec![vec![0.1, 0.2, 0.3], vec![-0.1, -0.2, -0.3]];
+//! process(&mut vec_data);
+//! assert_eq!(vec_data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
+//!
+//! let mut left = [0.1, 0.2, 0.3];
+//! let mut right = [-0.1, -0.2, -0.3];
+//! process(&mut [&mut left, &mut right]);
+//! assert_eq!(left, [0.5, 0.2, 0.3]);
+//! assert_eq!(right, [-0.5, -0.2, -0.3]);
+//!
+//! let mut noninterleaved = [0.1, 0.2, 0.3, -0.1, -0.2, -0.3];
+//! process(noninterleaved.chunks_mut(3));
+//! assert_eq!(noninterleaved, [0.5, 0.2, 0.3, -0.5, -0.2, -0.3]);
+//! ```
+//!
+//! This doesn't look too bad, does it?
+//!
+//! For your convenience, we're providing this trait here: [`ChannelMut`]
+//! (as well as its non-mutable counterpart [`Channel`]).
+//!
+//! If you want to make the function signature more concise (but also less explicit!),
+//! you can do this at the cost of defining yet another trait with a blanket implementation:
+//!
+//! ```
+//! trait ChannelMut<T>: AsMut<[T]> {}
+//! impl<T, U: AsMut<[T]> + ?Sized> ChannelMut<T> for &mut U {}
+//!
+//! trait ChannelsMut<T>: IntoIterator<Item: ChannelMut<T>> {}
+//! impl<T, U: IntoIterator<Item: ChannelMut<T>>> ChannelsMut<T> for U {}
+//!
+//! pub fn process(channels: impl ChannelsMut<f32>) {
+//!     for mut channel in channels {
+//!         let channel = channel.as_mut();
+//!         channel[0] *= 5.0;
+//!     }
+//! }
+//!
+//! let mut data = [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]];
+//! process(&mut data);
+//! assert_eq!(data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
+//!
+//! let mut noninterleaved = [0.1, 0.2, 0.3, -0.1, -0.2, -0.3];
+//! process(noninterleaved.chunks_mut(3));
+//! assert_eq!(noninterleaved, [0.5, 0.2, 0.3, -0.5, -0.2, -0.3]);
+//! ```
+//!
+//! TODO: ExactSizeIterator, see e.g. [`copy_to_interleaved()`] ...
 
 #![no_std]
 #![forbid(clippy::undocumented_unsafe_blocks)]
