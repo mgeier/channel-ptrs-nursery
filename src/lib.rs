5356503d4ab257bdd3c7ffcb10ac79a9fd3b71eb
Matthias Geier <Matthias.Geier@gmail.com>
2026-02-16 21:34:28 +0100

    minor docs update, remove ChannelsMut

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,8 @@
 //! ```
 //! pub fn process(channels: &mut [impl AsMut<[f32]>]) {
 //!     for channel in channels {
-//!         let channel = channel.as_mut();
+//!         // Specifying the type is not necessary, but we can be explicit if we want:
+//!         let channel: &mut [f32] = channel.as_mut();
 //!         channel[0] *= 5.0;
 //!     }
 //! }
@@ -74,7 +75,10 @@
 //! ... we can avoid this by adding more stuff ...
 //!
 //! ```
-//! pub fn process<'c, C: AsMut<[f32]> + ?Sized + 'c>(channels: impl IntoIterator<Item = &'c mut C>) {
+//! pub fn process<'c, C>(channels: impl IntoIterator<Item = &'c mut C>)
+//! where
+//!     C: AsMut<[f32]> + ?Sized + 'c,
+//! {
 //!     for mut channel in channels {
 //!         let channel = channel.as_mut();
 //!         channel[0] *= 5.0;
@@ -93,12 +97,14 @@
 //!
 //! ... this does what we want, but the function signature is getting quite cryptic ...
 //!
-//! ... let's try to make the function signature less cryptic by introducing a cryptic trait
-//! and a blanket implementation ...
+//! ... let's try to make the function signature less cryptic by introducing
+//! a trivial-looking trait and a cryptic blanket implementation ...
 //!
 //! ```
 //! trait ChannelMut<T>: AsMut<[T]> {}
 //! impl<T, U: AsMut<[T]> + ?Sized> ChannelMut<T> for &mut U {}
+//!
+//! // With this, the function signature arguably looks less intimidating:
 //!
 //! pub fn process(channels: impl IntoIterator<Item: ChannelMut<f32>>) {
 //!     for mut channel in channels {
@@ -130,8 +136,8 @@
 //!
 //! This doesn't look too bad, does it?
 //!
-//! For your convenience, we're providing this trait here: [`ChannelMut`]
-//! (as well as its non-mutable counterpart [`Channel`]).
+//! For your convenience, we're providing this trait (and the corresponding blanket implementation)
+//! here: [`ChannelMut`] (as well as its non-mutable counterpart [`Channel`]).
 //!
 //! If you want to make the function signature more concise (but also less explicit!),
 //! you can do this at the cost of defining yet another trait with a blanket implementation:
@@ -150,6 +156,8 @@
 //!     }
 //! }
 //!
+//! // This allows the same usage scenarios as before, we're trying just a few here.
+//!
 //! let mut data = [[0.1, 0.2, 0.3], [-0.1, -0.2, -0.3]];
 //! process(&mut data);
 //! assert_eq!(data, [[0.5, 0.2, 0.3], [-0.5, -0.2, -0.3]]);
@@ -160,6 +168,9 @@
 //! ```
 //!
 //! TODO: ExactSizeIterator, see e.g. [`copy_to_interleaved()`] ...
+//!
+//! TODO: Clone, DoubleEndedIterator, other traits ... users can create their own custom
+//! `Channels` and `ChannelsMut` traits (together with appropriate blanket implementations).
 
 #![no_std]
 #![forbid(clippy::undocumented_unsafe_blocks)]
@@ -480,10 +491,6 @@ unsafe extern "C" fn set_a_value(ptrs: *mut *mut f32, frames: usize, channels: u
         (*ptrs).write(99.9);
     }
 }
-
-// TODO: do we need those? docs: users can create their own traits (+ blanket impls)
-pub trait ChannelsMut<T>: IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<T>> {}
-impl<T, U: IntoIterator<IntoIter: ExactSizeIterator, Item: ChannelMut<T>>> ChannelsMut<T> for U {}
 
 impl Processor {
     // NB: This takes a mutable reference to `self` because it is *not* reentrant.
